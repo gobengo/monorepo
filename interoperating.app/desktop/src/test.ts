a51629951ae739e173efca5ae1e7bdc664bc7f5e
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-08-20 11:36:18 -0700

    add abortable run to interoperating.app/desktop

--- interoperating.app/desktop/src/test.ts
+++ interoperating.app/desktop/src/test.ts
@@ -1,5 +1,26 @@
 import test from 'ava';
 
-test('interoperating.app/desktop has a test', t => {
-	t.pass();
-});
+test('Can run app and then abort the run', async t => {
+	const abortController = new AbortController
+	await Promise.all([
+		run({ abort: abortController }),
+		timeout(1).then(() => abortController.abort())
+	])
+	t.assert(abortController.signal.aborted)
+})
+
+function run({ abort }: {
+	abort: {
+		signal: AbortSignal
+	}
+}) {
+	return new Promise((resolve, reject) => {
+		abort.signal.addEventListener('abort', resolve)
+	})
+}
+
+function timeout(milliseconds: number) {
+	return new Promise((resolve, reject) => {
+		setTimeout(resolve, milliseconds)
+	})
+}
