be40c045cfd5a5a6463181383491ee6325da83a7
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2023-01-11 01:53:22 -0800

    @activitypub.com/tauri init

---
+++ @activitypub.com/tauri/src-tauri/src/main.rs
@@ -0,0 +1,19 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello foo, {}! You've been greeted from Rust!", name)
+}
+
+fn main() {
+    let _port = portpicker::pick_unused_port().expect("failed to find unused port");
+
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
